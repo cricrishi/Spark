37d45b772700f188b777d4c5046ab029d424f652
atishaybaid <atishaybaid@gmail.com>
2017-08-21 23:13:37 +0530

    post method created

--- server.js
+++ server.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 
-
+var newData  = {
+      "key" : "lkjh"
+     
+   };
 
 app.get('/getKeys',function (req,res) {
     fs.readFile(__dirname + "/" + "keys.json", 'utf8', function (err, data) {
-       console.log( data );
        res.end( data );
    });
 
 });
+
+
+app.post('/addKeys',function(req,res){
+    fs.readFile(__dirname + "/"  + "keys.json","utf8",function(err,data){
+        console.log(data);
+        data = JSON.parse(data);
+        
+       data["user4"] = newData;
+       /* console.log( data );*/
+        res.end( JSON.stringify(data));
+
+    })
+})
 
 
 var server = app.listen(8081, function () {
