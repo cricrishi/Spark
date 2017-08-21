a737097cf9a4808d1ce63a70b3c8438b2894d740
atishaybaid <atishaybaid@gmail.com>
2017-08-21 22:33:17 +0530

    basic get method created

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+
+
+
+app.get('/getKeys',function (req,res) {
+    fs.readFile(__dirname + "/" + "keys.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+
+});
+
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
