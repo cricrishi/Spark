f9d78afef1545a9f7eca9df84033b66d4997d543
atishaybaid <atishaybaid@gmail.com>
2017-09-20 23:58:43 +0530

    push getting hit by node sdk

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ const express  = require('express');
 const app = express();
 const fs = require("fs");
 const bodyParser = require('body-parser');
+var admin = require("firebase-admin");
+//const webpush = require('web-push');
 var mongoose = require('mongoose');
+var serviceAccount = require("./serviceAccountKesy.json");
+
+
+//const vapidKeys = webpush.generateVAPIDKeys();
 
 
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/test';
 var db;
 
-
-var user = {
-   "user4" : {
-      "name" : "mohit",
-      "password" : "password4",
-      "profession" : "teacher",
-      "id": 4
-   }
-}
 
 
 
@@ -51,20 +48,12 @@ MongoClient.connect(url,options, function(err, c_db) {
           var port = server.address().port;
 
         console.log("Example app listening at http://%s:%s", host, port)
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://keeper-d3110.firebaseio.com"
+        });
         db.close();
     }); 
-    var dummyData = {
-      "name" : "mahesh",
-      "password" : "password1",
-      "profession" : "teacher",
-      "id": "1"
-   };
-
-   /* db.collection('users').save(dummyData,(err,result) =>{
-        console.log("saved to db")
-    })*/
-
-  console.log("Connected correctly to bro");
 
  
  
@@ -96,35 +85,37 @@ app.get('/listusers',function(req,res){
 
 app.get('/dropCl',function(req,res){
 
-    
-    db.collection('users').drop(function(err,suc){
-        if(err){
-            console.log(err);
-            return;
-        }
 
-        console.log("successfully droped collectoin");
-        db.close();
+    db.open(function(err,res){
+        db.collection('users').drop(function(err,suc){
+            if(err){
+                console.log(err);
+                return;
+            }
 
-    })
+            console.log("successfully droped collectoin");
+            db.close();
+
+        })
 
       res.send('users collectoin dropped');
 
 
-})
+
+    })
+    
+   
+
+
+});
 //api for adding user
 app.post('/addUser',function(req,res){
-    console.log("post called");
-   /* console.log(req);*/
-
-    console.log(req.body);
-
-    if(req.body){
+  
+  if(req.body){
      
      db.open(function(err,sucess){
 
         db.collection('users').insertOne(req.body,(err,result)=>{
-            //console.log(result);
             if (err) throw err;
             console.log("1 document inserted");
             db.close();
@@ -135,18 +126,52 @@ app.post('/addUser',function(req,res){
        
     }
 
-res.send('post message send');
-res.end();
-    /*fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       
-        data = JSON.parse( data );
-       data["user4"] = user["user4"];
-       console.log( data );
-       res.end( JSON.stringify(data));
+    res.send('post message send');
+    res.end();
+    
+
+})
 
 
-       res.end( data );
-    });*/
+app.get('/sendMsg',function(req,res){
+
+    /*db.open(function(err,res){
+
+        db.collection('users').find().toArray(function(err,result){
+            if(err){
+                console.log(err);
+                return;
+            }
+            console.log(result);
+            db.close();
+            
+
+        })
+
+       }) */
+        
+       //var registrationToken = "frcSHs5RCKw:APA91bH476UD0M8RdrUWaQmVy7cWoMIR1v8ojtQCrp6Cn5aqQ1O4bUTfimAkqu5uTGOkhvI6p06Ksai1n3roHrfdFUhdNdhDf-YUqvO4yOEwjdqqTx8NZzK4YZNzR7yX8DUITnhsCOCd";
+        var registrationToken ="f-dmZwaU3es:APA91bGH6UycV5GIplaf8j9-GEfe4RPMeaNSDEcRYzl-RBOQ4neYMAFHRgk5df7JGImWBirHM_wUqhJ7EUPbEt2hu8i8duc7VDMhVvjVE5G3WySsGnvXdKxAOthoqm0o4VBuQG8PMy91";     
+      var payload = {
+              data: {
+                message: "Push Message",
+                title: "Push Title"
+              }
+        }; 
+
+    admin.messaging().sendToDevice(registrationToken, payload)
+  .then(function(response) {
+    // See the MessagingDevicesResponse reference documentation for
+    // the contents of response.
+    console.log("Successfully sent message:", response);
+    res.end();
+  })
+  .catch(function(error) {
+    console.log("Error sending message:", error);
+    res.end();
+  });
+    
+
 
 })
 
